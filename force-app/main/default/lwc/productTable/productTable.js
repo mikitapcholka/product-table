310fdb911a276ab9f4d336b6571e589c427c3bff
MikitaPcholka <mikita.pcholka@gmail.com>
2025-05-03 22:51:06 +0300

    6 stages complete

--- force-app/main/default/lwc/productTable/productTable.js
+++ force-app/main/default/lwc/productTable/productTable.js
@@ -1,8 +1,16 @@
 import { track, LightningElement } from 'lwc';
 import getProducts from "@salesforce/apex/ProductTableController.getProducts";
-
+import addProduct from "@salesforce/apex/ProductTableController.addProduct";
+import editProduct from "@salesforce/apex/ProductTableController.editProduct";
+import deleteProduct from "@salesforce/apex/ProductTableController.deleteProduct";
 export default class ProductTable extends LightningElement {
     @track products;
+    @track isShowAddWindow = false;
+    @track isShowEditWindow = false;
+    @track isShowDeleteWindow = false;
+    @track currentProd;
+
+    //--------------------------------------------------------------------------------------
 
     connectedCallback() {
         getProducts()
@@ -13,4 +21,92 @@ export default class ProductTable extends LightningElement {
                 console.error(e);
             })
     }
+
+    handleChangeInput(e) {
+        this.currentProd[e.target.dataset.field] = e.target.value;
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    get options() {
+        return [
+            { label: 'Camera', value: 'Camera' },
+            { label: 'Phone', value: 'Phone' },
+            { label: 'Clock', value: 'Clock' },
+            { label: 'Tablet', value: 'Tablet' },
+        ];
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    showAddWindow() { 
+        this.currentProd = {
+            Id: 1,
+            Name: '',
+            Amount__c: '',
+            Price__c: '',
+            ProductType__c: '',
+            ReleaseDate__c: '',
+        };
+        this.isShowAddWindow = true;
+    }
+
+    showEditWindow(e) {
+        //this.currentProd = this.products.find(prod => prod.Id === e.target.dataset.id);
+        this.currentProd = JSON.parse(JSON.stringify(this.products.find(prod => prod.Id === e.target.dataset.id)));
+        this.isShowEditWindow = true;
+    }
+
+    showDeleteWindow(e) {  
+        this.currentProd = JSON.parse(JSON.stringify(this.products.find(prod => prod.Id === e.target.dataset.id)));
+        this.isShowDeleteWindow = true;
+    }
+
+    hideAddWindow() {  
+        this.isShowAddWindow = false;
+    }
+
+    hideEditWindow() {  
+        this.isShowEditWindow = false;
+    }
+
+    hideDeleteWindow() {  
+        this.isShowDeleteWindow = false;
+    }
+
+    //--------------------------------------------------------------------------------------
+
+    addRecord() {
+        addProduct({ addedRecord: JSON.stringify(this.currentProd) })
+            .then(result => {                
+                this.products = result;
+            })
+            .catch(e => {
+                console.error(e);
+            });
+        this.hideAddWindow();
+    }
+
+    editRecord() {
+        editProduct({ editedRecord: JSON.stringify(this.currentProd) })
+            .then(result => {                
+                this.products = result;
+            })
+            .catch(e => {
+                console.error(e);
+            });
+        this.hideEditWindow();
+    }
+
+    deleteRecord() {
+        deleteProduct({ deletedRecord: JSON.stringify(this.currentProd) })
+            .then(result => {                
+                this.products = result;
+            })
+            .catch(e => {
+                console.error(e);
+            });
+        this.hideDeleteWindow();  
+    }
+    
 }
